5bf4d8776e23548e07e9aa0071d195b496d3c3c5
Shubham <40598861+vijit101@users.noreply.github.com>
2025-03-22 18:12:40 +0530

    Added user routes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import productRouter from './src/features/product/product.routes.js';
+import userRouter from './src/features/user/user.routes.js';
 import bodyParser from 'body-parser';
+
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // to understand post json data
 //routes 
 app.use("/api/products",productRouter);
+app.use("/api/users",userRouter);
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Ecommerce API");
