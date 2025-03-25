c1e5a8b18208cae46654037f96058292eb798de7
Shubham <40598861+vijit101@users.noreply.github.com>
2025-03-25 10:55:36 +0530

    Added Cors package
    updated cors policy

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ import bodyParser from 'body-parser';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import CartItemRouter from './src/features/cart/cartitems.routes.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
+import cors from "cors";
+import loggerMiddleware from './src/middlewares/logger.middleware.js';
 
 const app = express();
+
+
+let corsOptions  = {
+    origin : "http//localhost:5500",
+    allowedHEaders:"*"
+}
+
+app.use(cors(corsOptions));
+
 
 app.use(bodyParser.json()); // to understand post json data
 //routes 
 app.use("/api-doc",swagger.serve,swagger.setup(apiDocs));
+app.use(loggerMiddleware);
 app.use("/api/users",userRouter);
 // app.use("/api/products",basicAuthorizer,productRouter);
 app.use("/api/products",jwtAuth,productRouter);
@@ -22,6 +34,24 @@ app.get("/",(req,res)=>{
     res.send("Welcome to Ecommerce API");
 })
 
+// only executes if other paths do not pick up as this middleware is for not defined apis should be at end 
+app.use((req,res)=>{
+    res.status(404).send("API not found. CHeck Documentation localhost:8080/api-docs");
+})
+
 app.listen(8080,()=>{
     console.log("server started at 8080");
 })
+
+
+
+// cors policy allows the api access only to a specific url without using cors library
+// app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin","http//localhost:5500"); // use "*" to allow access to all
+//     res.header("Access-Control-Allow-Headers","Content-Type,Authorization");// allowed headers to be send to server from client use "*" instead of cotnetntypeline
+//     //return ok for preflight request
+//     if(req.method == "OPTIONS"){
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
