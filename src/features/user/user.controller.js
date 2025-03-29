524da70f421ce152f61738666aaba8eacf0b8f65
Shubham <40598861+vijit101@users.noreply.github.com>
2025-03-29 11:18:29 +0530

    updated code for sign in
    using bycrypt to hash password

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,33 +1,40 @@
 import { UserModel } from "./user.model.js";
 import jwt from "jsonwebtoken";
 import UserRepository from "./user.repository.js";
-export class UserController{
+import bcrypt from "bcrypt";
+import { logger } from "../../middlewares/logger.middleware.js";
+export class UserController {
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+  async signUp(req, res) {
+    const { name, email, password, type } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12); // 12 is salt
+    let newuData = new UserModel(name, email, hashedPassword, type);
+    await this.userRepository.SignUp(newuData);
+    res.status(201).send(newuData);
+  }
 
-    constructor(){
-        this.userRepository = new UserRepository;
-    }
-    async signUp(req,res){
-        const {name,email,password,type} = req.body;
-        let newuData =  new UserModel(name,email,password,type);
-        await this.userRepository.SignUp(newuData);
-        res.status(201).send(newuData);
-    }
-    
-
-    async signIn(req,res,next){
-        try{
-            let result = await this.userRepository.SignIn(req.body.email,req.body.password);
-        if(!result){
-            return res.status(400).send("Incorrect Creds");
-        }else{
-            // send token 
-            const token = jwt.sign({userId:result.id,email:result.email},"iPB4VvT2pv3Ky66IxActwYaH5PbW5Pn9",{expiresIn:"1h"});
-            return res.status(200).send(token);
+  async signIn(req, res, next) {
+    try {
+      let user = await this.userRepository.FindByEmail(req.body.email);
+      if (!user) {
+        return res.status(400).send("Incorrect Email id");
+      } else {
+        // check if password for user right
+        const result = await bcrypt.compare(req.body.password, user.password);
+        if (result) {
+          const token = jwt.sign({ userId: result.id, email: result.email },"iPB4VvT2pv3Ky66IxActwYaH5PbW5Pn9",{ expiresIn: "1h" });
+          return res.status(200).send(token);
+        } else {
+          console.log(err);
+          return res.status(400).send("Somthing went wrong");
         }
-        }catch(err){
-            console.log(err);
-            return res.status(400).send("Somthing went wrong");
-        }
-        
+      }
+    } catch (err) {
+      console.log(err);
+      logger();
+      return res.status(400).send("Somthing went wrong");
     }
+  }
 }
--- src/features/user/user.repository.js
+++ src/features/user/user.repository.js
@@ -19,13 +19,27 @@ export default class UserRepository{
         
     }
 
-    async SignIn(email,password) {
+    // async SignIn(email,password) {
+    //     // get db
+    //     try {
+    //       const db = getDB();
+    //       const userCollection = db.collection("users");
+    //       // users.push(newUser);
+    //       return await userCollection.findOne({email:email,password:password});
+    //     } catch (err) {
+    //       console.log("user model sign up :" + err);
+    //       throw new ApplicationError("Something went wrong", 500);
+    //     }
+        
+    // }
+
+    async FindByEmail(email) {
         // get db
         try {
           const db = getDB();
           const userCollection = db.collection("users");
           // users.push(newUser);
-          return await userCollection.findOne({email:email,password:password});
+          return await userCollection.findOne({email:email});
         } catch (err) {
           console.log("user model sign up :" + err);
           throw new ApplicationError("Something went wrong", 500);
