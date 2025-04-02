647b4799e3eb5ad7f5d20cbd8c4675b9ecb4dcc3
Shubham <40598861+vijit101@users.noreply.github.com>
2025-04-02 12:31:54 +0530

    Function to create counter
    The counter is created so that id can be saved as 1,2,3,4 instead of obj id but obj id of mongo db is more powerful and globally unique

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -8,6 +8,9 @@ export const connectToMongoDB = ()=>{
     MongoClient.connect(url).then(client=>{
         mongoClient = client;
         console.log("mongodb is connected");
+        // this function creates counters cooecltion so we cna kind of map _id objectid to normal 1,2,3,4 etc ids
+        // remove this mexy line for normal mongo db id's that are globally unique always 
+        createCounter(client.db());
     }).catch(err=>{
         console.log(err);
     })
@@ -16,5 +19,12 @@ export const connectToMongoDB = ()=>{
 export const getDB=()=>{
     if(mongoClient!=null){
         return mongoClient.db();
+    }
+}
+
+const createCounter = async(db)=>{
+    const existingCounter = await db.collection("counters").findOne({_id:"cartItemId"});
+    if(!existingCounter){
+        await db.collection("counters").insertOne({_id:"cartItemId",value:0});
     }
 }
