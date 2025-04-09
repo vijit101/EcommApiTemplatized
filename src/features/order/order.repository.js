4e793df2faf6a9bf81456cd2a0fd652c8c7dd411
Shubham <40598861+vijit101@users.noreply.github.com>
2025-04-09 19:16:33 +0530

    order repo updated with order function
    - Added aggregate funciton to alculate quantity and price and then del the order details form cart hence adding a few details to order history

--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,13 +1,71 @@
-export default class OrderRepository{
+import { ObjectId } from "mongodb";
+import { getDB } from "../../config/mongodb.js";
+import OrderModel from "./order.model.js";
 
-    constructor(){
-        this.collections = "orders";
+export default class OrderRepository {
+  constructor() {
+    this.collections = "orders";
+  }
+  async placeOrder(userId) {
+    try {
+      const db = getDB();
+      let items = await this.getTotalAmount(userId);
+      const calTotalOrderAmt = items.reduce(
+        (acc, item) => acc + item.totalAmount,
+        0
+      );
+      const newOrder = new OrderModel(new ObjectId(userId),calTotalOrderAmt,new Date());
+      await db.collection(this.collections).insertOne(newOrder);
+      // stock update as ordered 
+      for(let item of items){
+        await db.collection("products").updateOne({
+          _id : item.productId
+        },{$inc:{stock: -item.quantity}}); 
+      }
+      // delete cart items as now its an orderitem
+      await db.collection("cartItems").deleteMany({
+        userId: new ObjectId(userId),
+      });
+      return newOrder;
+    } catch (err) {
+      console.log("Error placing order:", err);
     }
-    async placeOrder(userId){
-        try{
-            
-        }catch(err){
-            
-        }
-    }
+  }
+
+  async getTotalAmount(userId) {
+    const db = getDB();
+
+    const items = await db
+      .collection("cartItems")
+      .aggregate([
+        {
+          $match: {
+            userId: new ObjectId(userId),
+          },
+        },
+        // get the products via products collection using product id in cart
+        {
+          $lookup: {
+            from: "products",
+            localField: "productId",
+            foreignField: "_id",
+            as: "productInfo",
+          },
+        },
+        //3 unwinding product info
+        {
+          $unwind: "$productInfo",
+        },
+        //4. cal total amount for each cart item
+        {
+          $addFields: {
+            totalAmount: {
+              $multiply: ["$productInfo.price", "$quantity"],
+            },
+          },
+        },
+      ])
+      .toArray();
+    return items;
+  }
 }
