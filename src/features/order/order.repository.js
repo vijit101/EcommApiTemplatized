db6414fec97a776f37bc6f337665dbbcd1fca378
Shubham <40598861+vijit101@users.noreply.github.com>
2025-04-09 10:25:27 +0530

    Order repo
    order base line code setup

---
+++ src/features/order/order.repository.js
@@ -0,0 +1,13 @@
+export default class OrderRepository{
+
+    constructor(){
+        this.collections = "orders";
+    }
+    async placeOrder(userId){
+        try{
+            
+        }catch(err){
+            
+        }
+    }
+}
