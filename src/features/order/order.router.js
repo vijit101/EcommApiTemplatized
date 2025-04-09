9991226b3a52af3f162ee160cb79d8ce66b999f0
Shubham <40598861+vijit101@users.noreply.github.com>
2025-04-09 19:14:39 +0530

    order model , controller , router set

--- src/features/order/order.router.js
+++ src/features/order/order.router.js
@@ -4,8 +4,8 @@ import OrderController from "./order.controller.js";
 const orderRouter = express.Router();
 const orderController = new OrderController();
 
-orderRouter.get('/',(req,res)=>{
-    
+orderRouter.get('/',(req,res,next)=>{
+    orderController.placeOrder(req,res,next);
 });
 
 orderRouter.post("/",(req,res)=>{
