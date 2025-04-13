09c3cb162399c0485f2bcfce81dc20902ecc8817
Shubham <40598861+vijit101@users.noreply.github.com>
2025-04-13 19:09:46 +0530

    updated user schema

--- src/features/user/user.schema.js
+++ src/features/user/user.schema.js
@@ -1,9 +1,23 @@
 import { Schema } from "mongoose";
 const typeEnum = ["Customer", "Seller"];
 
+var passwordregex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
 export const userSchema = new Schema({
   name: String,
-  email: { type: String, unique: true },
-  password: String,
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+\@.+\../, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    validate: {
+      validator: function (vals) {
+        return passwordregex.test(vals);
+      },
+      message: "password should be btw 6 = 16 chars ",
+    },
+  },
   type: { type: String, enum: typeEnum },
 });
